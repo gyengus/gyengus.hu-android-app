fc3123d2e8ae9214ada9dbd4ec43a0fd3ecde6d9
gyengus <gyengus@gmail.com>
2019-10-23 14:56:49 +0200

    Change Google Analytics to Firebase

--- app/src/main/java/hu/gyengus/web/MainActivity.java
+++ app/src/main/java/hu/gyengus/web/MainActivity.java
@@ -1,7 +1,3 @@
-/**
- * Created by gyengus
- */
-
 package hu.gyengus.web;
 
 import android.app.Activity;
@@ -10,25 +6,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
-//import android.net.http.SslError;
 import android.webkit.WebViewClient;
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.HitBuilders;
-import com.google.android.gms.analytics.Tracker;
-//import android.util.Log;
 import android.widget.ProgressBar;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
 
-//public class MainActivity extends ActionBarActivity {
+
 public class MainActivity extends Activity {
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     private WebView webView;
     private ProgressBar mPbar;
@@ -36,24 +27,17 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        // Get tracker.
-        Tracker t = ((MyApplication) this.getApplication()).getTracker(MyApplication.TrackerName.APP_TRACKER);
-        // Enable Advertising Features.
-        t.enableAdvertisingIdCollection(true);
-
-        // Set screen name.
-        t.setScreenName(getString(R.string.main_screen_name));
-
-        // Send a screen view.
-        t.send(new HitBuilders.AppViewBuilder().build());
-        //Log.i("Tracker: ", t.toString());
-
         mPbar = (ProgressBar) findViewById(R.id.progressBar);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN, null);
 
         webView = (WebView) findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
@@ -62,20 +46,17 @@ public class MainActivity extends Activity {
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (Uri.parse(url).getHost().equals(getString(R.string.domain))) {
-                    // This is my web site, so do not override; let my WebView load the page
-                    return false;
+                if (url != null) {
+                    if (Uri.parse(url).getHost().equals(getString(R.string.domain))) {
+                        // This is my web site, so do not override; let my WebView load the page
+                        return false;
+                    }
+                    // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
                 }
-                // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(intent);
                 return true;
             }
-
-            /*@Override
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                handler.proceed(); // Ignoráljuk a certificate problémáit, enélkül nem fogadja el a self signed certet
-            }*/
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
@@ -119,15 +100,11 @@ public class MainActivity extends Activity {
     @Override
     public void onStart() {
         super.onStart();
-        //GoogleAnalytics.getInstance(this).reportActivityStart(this);
-        //Log.i("Analytics", "ActivityStart");
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        //GoogleAnalytics.getInstance(this).reportActivityStop(this);
-        //Log.i("Analytics", "ActivityStop");
     }
 
 }
